86049a1e1ca02c156ee75037da9be6a3628ff80f
FXD <69631895+andyfu35@users.noreply.github.com>
2021-12-15 09:15:17 +0800

    Add files via upload

--- homework.py
+++ homework.py
@@ -6,11 +6,12 @@ root = Tk()
 root.geometry('+200+200')
 mycanvas = Canvas(root, width=1000, height=1000)
 mycanvas.pack()
-T, r, h, k, x, y = 150, 150, 0, 0, 500, 500
-def Diameter(theta):
+T, r, h, k, x, y = 150, 150, 0, 0, 500, 500     #可更改參數 T = 圖形初始角度 r = 圓形路徑中的半徑 x,y = 初始位置
+def Diameter(theta):                            #徑度主換
     diameter = theta*np.pi/180
     return diameter
 
+# pincers 1~4 分別對映著螯的前端、中端、左螯、右螯
 Lpincers_1 = mycanvas.create_line(x, y, x - 125*np.cos(Diameter(T+40)), y - 125*np.sin(Diameter(T+40)), width=20, fill='red')
 Lpincers_2 = mycanvas.create_line(x - 125*np.cos(Diameter(T+40)), y - 125*np.sin(Diameter(T+40)), x - 200*np.cos(Diameter(T+70)), y - 200*np.sin(Diameter(T+70)), width=20, fill='red')
 Lpincers_3 = mycanvas.create_polygon(x - 200*np.cos(Diameter(T+70)), y - 200*np.sin(Diameter(T+70)), x - 250*np.cos(Diameter(T+75)), y - 250*np.sin(Diameter(T+75)), x - 275*np.cos(Diameter(T+80)), y - 275*np.sin(Diameter(T+80)), fill='red')
@@ -30,12 +31,12 @@ leg_22 =  mycanvas.create_line(x + 125*np.cos(Diameter(T+35)), y + 125*np.sin(Di
 leg_32 =  mycanvas.create_line(x + 125*np.cos(Diameter(T-25)), y + 125*np.sin(Diameter(T-25)), x + 160*np.cos(Diameter(T-10)), y + 160*np.sin(Diameter(T-10)), width=10, fill='red')
 main_body = mycanvas.create_oval(x-50, y-50, x+50, y+50, fill='red')
 
-for x in trange(-150,150):
-    T = T-180/300
-    y = (r**2 - (x-h)**2)**0.5 + k
+for x in trange(-150,150):          # -150~150為可更改參數，不可大於r (r^2 >= 150^2)
+    T = T-180/300                   #隨圓周運動時轉換自身身體角度
+    y = (r**2 - (x-h)**2)**0.5 + k  #圓周運動公式，可更換任意曲線公式
     x = x+500
     y = y+500
-
+                    #清除初始化位置
     mycanvas.delete(Lpincers_1), mycanvas.delete(Lpincers_2), mycanvas.delete(Lpincers_3), mycanvas.delete(Lpincers_4)
     mycanvas.delete(Rpincers_1), mycanvas.delete(Rpincers_2), mycanvas.delete(Rpincers_3), mycanvas.delete(Rpincers_4)
     mycanvas.delete(leg_1), mycanvas.delete(leg_2), mycanvas.delete(leg_3)
@@ -43,7 +44,7 @@ for x in trange(-150,150):
     mycanvas.delete(leg_12), mycanvas.delete(leg_22), mycanvas.delete(leg_32)
     mycanvas.delete(main_body)
 
-    if x % 2 == 0:
+    if x % 2 == 0:          #動作狀態一，可視自身需求更改動作
         Lpincers_1 = mycanvas.create_line(x, y, x - 125*np.cos(Diameter(T+50)), y - 125*np.sin(Diameter(T+50)), width=20, fill='red')
         Lpincers_2 = mycanvas.create_line(x - 125*np.cos(Diameter(T+50)), y - 125*np.sin(Diameter(T+50)), x - 200*np.cos(Diameter(T+80)), y - 200*np.sin(Diameter(T+80)), width=20, fill='red')
         Lpincers_3 = mycanvas.create_polygon(x - 200*np.cos(Diameter(T+80)), y - 200*np.sin(Diameter(T+80)), x - 250*np.cos(Diameter(T+80)), y - 250*np.sin(Diameter(T+80)), x - 275*np.cos(Diameter(T+85)), y - 275*np.sin(Diameter(T+85)), fill='red')
@@ -62,7 +63,7 @@ for x in trange(-150,150):
         leg_22 =  mycanvas.create_line(x + 125*np.cos(Diameter(T+35)), y + 125*np.sin(Diameter(T+35)), x + 160*np.cos(Diameter(T+50)), y + 160*np.sin(Diameter(T+50)), width=10, fill='red')
         leg_32 =  mycanvas.create_line(x + 125*np.cos(Diameter(T-25)), y + 125*np.sin(Diameter(T-25)), x + 160*np.cos(Diameter(T-10)), y + 160*np.sin(Diameter(T-10)), width=10, fill='red')
         main_body = mycanvas.create_oval(x-50, y-50, x+50, y+50, fill='red')
-    else:
+    else:                   #動作狀態二，可視自身需求更改動作
         Lpincers_1 = mycanvas.create_line(x, y, x - 125*np.cos(Diameter(T+40)), y - 125*np.sin(Diameter(T+40)), width=20, fill='red')
         Lpincers_2 = mycanvas.create_line(x - 125*np.cos(Diameter(T+40)), y - 125*np.sin(Diameter(T+40)), x - 200*np.cos(Diameter(T+70)), y - 200*np.sin(Diameter(T+70)), width=20, fill='red')
         Lpincers_3 = mycanvas.create_polygon(x - 200*np.cos(Diameter(T+70)), y - 200*np.sin(Diameter(T+70)), x - 250*np.cos(Diameter(T+75)), y - 250*np.sin(Diameter(T+75)), x - 275*np.cos(Diameter(T+80)), y - 275*np.sin(Diameter(T+80)), fill='red')
@@ -83,4 +84,4 @@ for x in trange(-150,150):
         main_body = mycanvas.create_oval(x-50, y-50, x+50, y+50, fill='red')
 
     mycanvas.update()
-    mycanvas.after(500)
+    mycanvas.after(500) #delay 500毫秒
